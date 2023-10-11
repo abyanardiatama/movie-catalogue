97275b7fcc9154619772472045bba16754192040
abyanardiatama <abyanardiatama@students.undip.ac.id>
2023-10-11 07:45:25 +0700

    make application shell

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '3925b586c3206aa8b3eac6dedf3aacea',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
