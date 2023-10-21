838158dd3f1c8f849b379b2a9eb4af4abf740141
abyanardiatama <abyanardiatama@students.undip.ac.id>
2023-10-21 09:05:56 +0700

    add indexedDb

--- src/scripts/views/pages/now-playing.js
+++ src/scripts/views/pages/now-playing.js
@@ -7,6 +7,7 @@ const NowPlaying = {
       <div class="content">
         <h2 class="content__heading">Now Playing in Cinema</h2>
         <div id="movies" class="movies">
+
         </div>
       </div>
     `;
