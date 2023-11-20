86066ad219d3c6698cdacffe59ff5a2311833b48
abyanardiatama <abyanardiatama@students.undip.ac.id>
2023-11-20 22:49:13 +0700

    add testing

--- src/scripts/utils/websocket-initiator.js
+++ src/scripts/utils/websocket-initiator.js
@@ -1,19 +1,20 @@
-import CONFIG from "../globals/config";
-import NotificationHelper from "./notification-helper";
+import NotificationHelper from './notification-helper';
+import CONFIG from '../globals/config';
 
 const WebSocketInitiator = {
   init(url) {
     const webSocket = new WebSocket(url);
     webSocket.onmessage = this._onMessageHandler;
   },
+
   _onMessageHandler(message) {
     const movie = JSON.parse(message.data);
     NotificationHelper.sendNotification({
-        title: `${movie.title} is on cinema!`,
-        options: {
-            body: movie.overview,
-            image: `${CONFIG.BASE_IMAGE_URL + movie.poster_path}`,
-        },
+      title: `${movie.title} is on cinema!`,
+      options: {
+        body: movie.overview,
+        image: `${CONFIG.BASE_IMAGE_URL + movie.poster_path}`,
+      },
     });
   },
 };
